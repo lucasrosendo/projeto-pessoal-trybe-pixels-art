848468f63ec61ed179a46a75814aed4aa7e215af
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2021-06-06 02:20:24 -0300

    quebrando a cabeca

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+//Determinando as cores principais
+let cor1 = document.getElementsByClassName("color")[0]
+cor1.style.background = "black"
+let cor2 = document.getElementsByClassName("color")[1]
+cor2.style.background = "blue"
+let cor3 = document.getElementsByClassName("color")[2]
+cor3.style.background = "red"
+let cor4 = document.getElementsByClassName("color")[3]
+cor4.style.background = "gray"
 
-
+//Criando a tabela
+//Criar div com a classe "pixel"
